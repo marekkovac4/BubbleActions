3df2d072853bc2017e30b7d15d9b133f59318b1c
marek.kovac <marek.kovac.4@gmail.com>
2019-03-13 15:34:32 +0100

    params for custom textview

--- bubbleactions/src/main/java/me/samthompson/bubbleactions/BubbleActionOverlay.java
+++ bubbleactions/src/main/java/me/samthompson/bubbleactions/BubbleActionOverlay.java
@@ -20,7 +20,6 @@ import android.view.animation.Interpolator;
 import android.view.animation.OvershootInterpolator;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 /**
@@ -158,7 +157,10 @@ class BubbleActionOverlay extends FrameLayout implements TextCallback {
 
     public void setCustomTextView(TextView textView) {
         selectedItemTextView = textView;
-        addView(selectedItemTextView, textView.getLayoutParams());
+        if (textView.getLayoutParams() == null)
+            addView(selectedItemTextView, new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        else
+            addView(selectedItemTextView, textView.getLayoutParams());
     }
 
     void setLabelTypeface(Typeface typeface) {
